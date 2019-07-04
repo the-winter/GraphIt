2699e3aa4c96b9d6ac73695e5f0f07f93c13205e
thewinter <19483860+the-winter@users.noreply.github.com>
2019-07-04 14:37:06 +0800

    can graph input

--- src/hello.js
+++ src/hello.js
@@ -7,30 +7,24 @@ const draw = () => {
     console.log(document.getElementById("title").value);
     const title = document.getElementById("title").value;
 
-    const one = document.getElementById("one").value;
-    const two = document.getElementById("two").value;
-    const three = document.getElementById("three").value;
-    // const title = document.getElementById("title").value;
-    // const title = document.getElementById("title").value;
-    // const title = document.getElementById("title").value;
-    // const title = document.getElementById("title").value;
+    const one = parseInt(document.getElementById("one").value);
+    const two = parseInt(document.getElementById("two").value);
+    const three = parseInt(document.getElementById("three").value);
+    const four = parseInt(document.getElementById("four").value);
+    const five = parseInt(document.getElementById("five").value);
+    const six = parseInt(document.getElementById("six").value);
+    const seven = parseInt(document.getElementById("seven").value);
+
+    const inputs = [[1, one], [2, two], [3, three], [4, four], [5, five], [6, six], [7, seven]]
     var g = new Dygraph(
         document.getElementById('graph'),
-        [
-            [1, 4, 3],
-            [2, 2, 2],
-            [3, null, 7],
-            [4, 5, null],
-            [5, null, 5],
-            [6, 3, null],
-            [7, null, 6]
-        ],
+        inputs,
         {
             // showRoller: true,
             // customBars: true,
             title: title,
             ylabel: "test",
-            labels: ['x', 'A', 'B'],
+            labels: ['x', 'A'],
             connectSeparatedPoints: true,
             drawPoints: true
         }
