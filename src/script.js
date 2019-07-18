06f14c8af70e3d91f0c07ea0349b4cb8fd8cd924
thewinter <19483860+the-winter@users.noreply.github.com>
2019-07-18 12:45:16 +0800

    both axes graphed fix

--- src/script.js
+++ src/script.js
@@ -4,20 +4,22 @@ $(document).ready(function () {
     var axis2Wrapper;
     const addButton = $('button.add-inputs');
     var x = 3;
-    let fieldHTML;
+    let axis1fieldHtml;
+    let axis2fieldHtml;
 
 
     //Once add button is clicked
     $(addButton).on('click', function (e) {
         axis1Wrapper = $(this).closest('.individual-axis-inputs').find('.added-inputs')
-        axis2Wrapper = $(this).closest('ul').find('.individual-axis-inputs2')
+        axis2Wrapper = $(this).closest('ul').find('.individual-axis-inputs2').find('.added-inputs')
         e.stopImmediatePropagation();
         //Check maximum number of input fields
         if (x < maxField) {
-            fieldHTML = `<div><label>${x}</label><input id="${x}"type="text" name="field_name[]" value=""/><a href="javascript:void(0);" class="remove_button">X</a></div>`
+            axis1fieldHtml = `<div><label>${x}</label><input id="${x}"type="text" name="field_name[]" value=""/><a href="javascript:void(0);" class="remove_button">X</a></div>`
+            axis2fieldHtml = `<div><label>${x}</label><input id="${x}B"type="text" name="field_name[]" value=""/><a href="javascript:void(0);" class="remove_button">X</a></div>`
             x++; //Increment field counter
-            $(axis1Wrapper).append(fieldHTML); //Add field html
-            $(axis2Wrapper).append(fieldHTML);
+            $(axis1Wrapper).append(axis1fieldHtml); //Add field html
+            $(axis2Wrapper).append(axis2fieldHtml);
         }
     });
 })
